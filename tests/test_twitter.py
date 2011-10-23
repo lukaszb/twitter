52b99157a0d7c671e52caf6da636ef3710b71a78
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2011-10-23 19:35:17 +0200

    Added sanity test for special __method paramter

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -1,5 +1,7 @@
+import urlparse
 import unittest
 from twitter import Twitter
+from twitter import OAuth
 from twitter.api import urllib_request
 
 
@@ -19,4 +21,12 @@ class TestTwitter(unittest.TestCase):
         request = self.twitter.foo.bar(__request_only=True, __method='POST',
             foo='bar')
         self.assertEqual(request.get_method(), 'POST')
+
+    def test_calls_respect_method_argument_and_it_is_not_passed_with_oauth(self):
+        twitter = Twitter(auth=OAuth('token', 'secret', 'consumer_token',
+            'consumer_token_secret'))
+        request = twitter.foo.bar(__request_only=True, __method='POST',
+            foo='bar')
+        data = urlparse.parse_qs(request.get_data())
+        self.assertFalse('__method' in data)
 
